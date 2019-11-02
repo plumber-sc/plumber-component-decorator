13213b4b4d864ad6fd139bb551ef98d3fecc4022
Erwin Werkman <werkman.erwin@gmail.com>
2019-11-03 00:20:30 +0100

    Fixed issue with build

--- Build.ps1
+++ Build.ps1
@@ -7,4 +7,4 @@ Write-Host $Env:APPVEYOR_BUILD_VERSION
 Write-Host $version
 
 dotnet build .\src\Plugin.Plumber.Component.Decorator\Plugin.Plumber.Component.Decorator.csproj
-dotnet pack .\src\Plugin.Plumber.Component.Decorator\Plugin.Plumber.Component.Decorator.csproj -c Release -o ..\..\artifacts -p:Version=$version --no-build --no-dependencies
+dotnet pack .\src\Plugin.Plumber.Component.Decorator\Plugin.Plumber.Component.Decorator.csproj -c Release -o ..\..\artifacts -p:Version=$version
