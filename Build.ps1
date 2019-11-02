b5193f810d7a1232982f21d2a250b3a1f6825f79
Erwin Werkman <werkman.erwin@gmail.com>
2019-11-03 00:16:38 +0100

    Fixed nuget package

--- Build.ps1
+++ Build.ps1
@@ -1,8 +1,10 @@
-.\Lib\GitVersion.exe /output buildserver
+$str = GitVersion.exe | out-string
+$json = ConvertFrom-Json $str
+$version = $json.NuGetVersionV2
 
 Write-Host "Building:"
 Write-Host $Env:APPVEYOR_BUILD_VERSION
-Write-Host $Env:GitVersion_NuGetVersion
+Write-Host $version
 
 dotnet build .\src\Plugin.Plumber.Component.Decorator\Plugin.Plumber.Component.Decorator.csproj
-dotnet pack .\src\Plugin.Plumber.Component.Decorator\Plugin.Plumber.Component.Decorator.csproj -c Release -o ..\..\artifacts /p:Version=$Env:GitVersion_NuGetVersion -p:NuspecFile=.\src\Plugin.Plumber.Component.Decorator\Plugin.Plumber.Component.Decorator.nuspec
+dotnet pack .\src\Plugin.Plumber.Component.Decorator\Plugin.Plumber.Component.Decorator.csproj -c Release -o ..\..\artifacts -p:Version=$version --no-build --no-dependencies
